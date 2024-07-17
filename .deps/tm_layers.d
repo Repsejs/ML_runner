7d448e0a3a365c5f224b4fdf8369a67e8f9ff323
Repsejs <repsejs@hotmail.com>
2024-07-17 14:09:34 +0200

    tinymaix compiles

---
+++ .deps/tm_layers.d
@@ -0,0 +1,11 @@
+build/tm_layers.o: lib/tinymaix/src/tm_layers.c \
+ lib/tinymaix/include/tinymaix.h lib/tinymaix/include/tm_port.h \
+ lib/tinymaix/include/arch_cpu.h lib/tinymaix/include/tinymaix.h
+
+lib/tinymaix/include/tinymaix.h:
+
+lib/tinymaix/include/tm_port.h:
+
+lib/tinymaix/include/arch_cpu.h:
+
+lib/tinymaix/include/tinymaix.h:
---
+++ .deps/tm_model.d
@@ -0,0 +1,6 @@
+build/tm_model.o: lib/tinymaix/src/tm_model.c \
+ lib/tinymaix/include/tinymaix.h lib/tinymaix/include/tm_port.h
+
+lib/tinymaix/include/tinymaix.h:
+
+lib/tinymaix/include/tm_port.h:
